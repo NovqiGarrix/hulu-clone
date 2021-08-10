4f34eeb27ab464b19339e1598beb2f75210c0132
NovqiGarrix <novqigarrix@gmail.com>
2021-08-10 12:04:18 +0700

    .Next

---
+++ .next/static/xIhS3K-r7ZUjxA-y4jlCA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/753-6d78b11b5c540d506887.js","static/chunks/pages/index-89f93d8c03bb04d1e18c.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
